d619c7dee85375bc25cf274c9f2b0cdb0afd0fdc
王裕响 <wb-wyx657769@alibaba-inc.com>
2020-11-17 16:22:30 +0800

    切片slice扩容规则

--- slice/slice_basic.go
+++ slice/slice_basic.go
@@ -39,6 +39,22 @@ func main()  {
 	testArrayPoint(&arrayD) //传切片 func Array : 0xc000004500 , [1 102]
 	fmt.Printf("arrayC : %p , %v\n", &arrayC, arrayC) // arrayC : 0xc0000044c0 , [1 202]
 
+	fmt.Println("===================")
+	testSliceAppend()
+
+	
+}
+
+//Go 中切片扩容的策略:
+//如果切片的容量小于 1024 个元素，于是扩容的时候就翻倍增加容量。一旦元素个数超过 1024 个元素，那么增长因子就变成 1.25 ，即每次增加原来容量的四分之一。
+func testSliceAppend() {
+	slice := []int{10, 20, 30, 40}
+	newSlice := append(slice, 50)
+	fmt.Printf("Before slice = %v, Pointer = %p, len = %d, cap = %d\n", slice, &slice, len(slice), cap(slice))
+	fmt.Printf("Before newSlice = %v, Pointer = %p, len = %d, cap = %d\n", newSlice, &newSlice, len(newSlice), cap(newSlice))
+	newSlice[1] += 10
+	fmt.Printf("After slice = %v, Pointer = %p, len = %d, cap = %d\n", slice, &slice, len(slice), cap(slice))
+	fmt.Printf("After newSlice = %v, Pointer = %p, len = %d, cap = %d\n", newSlice, &newSlice, len(newSlice), cap(newSlice))
 }
 
 func testArrayPoint(x *[]int) {
