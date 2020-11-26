8a66675eaa8a8df1e676a99afd6fa8177087ed34
王裕响 <wb-wyx657769@alibaba-inc.com>
2020-11-26 11:19:50 +0800

    条件语句select

--- process_control/process.go
+++ process_control/process.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 /**
 条件语句if:
@@ -41,6 +44,34 @@ Type Switch:
 		default: //可选
 			statement(s)
 	}
+
+条件语句select
+select 语句类似于 switch 语句，但是select会随机执行一个可运行的case。如果没有case可运行，它将阻塞，直到有case可运行
+select 语句的语法如下:
+	select {
+    case communication clause  :
+       statement(s);
+    case communication clause  :
+       statement(s);
+    // 你可以定义任意数量的 case
+	default : //可选
+	statement(s);
+	}
+PS:
+	每个case都必须是一个通信
+    所有channel表达式都会被求值
+    所有被发送的表达式都会被求值
+    如果任意某个通信可以进行，它就执行；其他被忽略。
+    如果有多个case都可以运行，Select会随机公平地选出一个执行。其他不会执行。
+    否则：
+    如果有default子句，则执行该语句。
+    如果没有default字句，select将阻塞，直到某个通信可以运行；Go不会重新对channel或值进行求值。
+select由比较多的限制，其中最大的一条限制就是每个case语句里必须是一个IO操作
+在一个select语句中，Go会按顺序从头到尾评估每一个发送和接收的语句。
+如果其中的任意一个语句可以继续执行（即没有被阻塞），那么就从那些可以执行的语句中任意选择一条来使用。
+如果没有任意一条语句可以执行（即所有的通道都被阻塞），那么有两种可能的情况：
+①如果给出了default语句，那么就会执行default的流程，同时程序的执行会从select语句后的语句中恢复。
+②如果没有default语句，那么select语句将被阻塞，直到至少有一个case可以进行下去。
  */
 func main() {
 	fmt.Println("==========if============")
@@ -49,7 +80,44 @@ func main() {
 	switchDemo()
 	fmt.Println("==============Type Switch===========")
 	typeSwitchDemo()
+	fmt.Println("==========select==============")
+	selectDemo()
+	fmt.Println("============select time out ==========")
+	selectTimeOutDemo()
 
+}
+var resChan = make(chan int)
+func selectTimeOutDemo() {
+	select {
+	case data := <-resChan:
+		doData(data)
+	case <-time.After(time.Second * 3):
+		fmt.Println("request time out")
+	}
+}
+func doData(data int) {
+	//...
+	fmt.Println("---doData---")
+}
+
+func selectDemo() {
+	var c1, c2, c3 chan int //一个可以发送 int 类型数据的 channel 一般写为 chan int
+	var i1, i2 int
+	select { //不停的在这里检测
+	case i1 = <-c1: //检测有没有数据可以读, 如果cl成功读取到数据，则进行该case处理语句 //阻塞接收数据
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2: //检测有没有可以写，如果成功向c2写入数据，则进行该case处理语句
+		fmt.Printf("sent ", i2, " to c2\n")
+	case i3, ok := (<-c3):  // same as: i3, ok := <-c3 //非阻塞接收数据
+		if ok {
+			fmt.Printf("received ", i3, " from c3\n")
+		} else {
+			fmt.Printf("c3 is closed\n")
+		}
+	default: //假如没有default，那么在以上两个条件都不成立的情况下，就会在此阻塞//一般default会不写在里面，select中的default子句总是可运行的，因为会很消耗CPU资源
+		//如果以上都没有符合条件，那么则进行default处理流程
+		fmt.Printf("no communication\n")
+	}
 }
 
 func typeSwitchDemo() {
