08513cebd4e25bd1ab4df1857d5ec7c17ece5e65
王裕响 <wb-wyx657769@alibaba-inc.com>
2020-12-07 16:28:56 +0800

    函数的 panic 异常处理

--- function1/function_demo.go
+++ function1/function_demo.go
@@ -74,6 +74,29 @@ defer用途：
 	1. 关闭文件句柄
     2. 锁资源释放
     3. 数据库连接释放
+
+异常处理：
+Golang 没有结构化异常，使用 panic 抛出错误，recover 捕获错误。
+场景简单描述：Go中可以抛出一个panic的异常，然后在defer中通过recover捕获这个异常，然后正常处理。
+panic：
+	1、内置函数
+    2、假如函数F中书写了panic语句，会终止其后要执行的代码，在panic所在函数F内如果存在要执行的defer函数列表，按照defer的逆序执行
+    3、返回函数F的调用者G，在G中，调用函数F语句之后的代码不会执行，假如函数G中存在要执行的defer函数列表，按照defer的逆序执行
+    4、直到goroutine整个退出，并报告错误
+recover：
+    1、内置函数
+    2、用来控制一个goroutine的panicking行为，捕获panic，从而影响应用的行为
+    3、一般的调用建议
+        a). 在defer函数中，通过recever来终止一个goroutine的panicking过程，从而恢复正常代码的执行
+        b). 可以获取通过panic传递的error
+PS：
+    1.利用recover处理panic指令，defer 必须放在 panic 之前定义，另外 recover 只有在 defer 调用的函数中才有效。否则当panic时，recover无法捕获到panic，无法防止panic扩散。
+    2.recover 处理异常后，逻辑并不会恢复到 panic 那个点去，函数跑到 defer 之后的那个点。
+    3.多个 defer 会形成 defer 栈，后定义的 defer 语句会被最先调用。
+由于 panic、recover 参数类型为 interface{}，因此可抛出任何类型对象。
+延迟调用中引发的错误，可被后续延迟调用捕获，但仅最后一个错误可被捕获。
+区别使用 panic 和 error 两种方式:
+	导致关键流程出现不可修复性错误的使用 panic，其他使用 error。
 */
 func test(fn func() int) int {
 	return fn()
@@ -140,6 +163,67 @@ func main() {
 	closureDemo()
 	fmt.Println("=============defer")
 	deferDemo()
+	fmt.Println("==============error")
+	errorDemo()
+	fmt.Println("=============error01")
+	errorDemo1()
+	fmt.Println("===============error02")
+	errorDemo2()
+	fmt.Println("================实现类似try-catch")
+	tryCatchErrorDemo()
+}
+
+func Try(fun func(), handler func(interface{})) {
+	defer func() {
+		if err := recover();err!= nil {
+			handler(err)
+		}
+	}()
+	fun()
+}
+
+func tryCatchErrorDemo() {
+	Try(func() {
+		panic("test try-catch demo")
+	}, func(i interface{}) {
+		fmt.Println("try-catch error:", i)
+	})
+}
+
+func errorDemo2() {
+	defer func() {
+		fmt.Println("demo2--", recover())
+	}()
+
+	defer func() {
+		panic("demo2-- defer panic")
+	}()
+
+	panic("demo2-- test panic")
+}
+
+func errorDemo1() {
+	//向已关闭的通道发送数据会引发panic
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("----->", err)  //-----> send on closed channel
+		}
+	}()
+
+	var ch chan int = make(chan int, 10)
+	close(ch)
+	ch <- 1
+
+}
+
+func errorDemo() {
+	defer func() {
+		if err := recover(); err != nil {
+			println(err.(string)) // 将 interface{} 转型为具体类型。
+		}
+	}()
+
+	panic("panic error!")
 }
 
 func deferDemo() {
